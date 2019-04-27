0599969b9acc488d3918104283d74cdd6188effb
Tomas Roj <rojtomas@email.cz>
2019-04-27 21:19:40 +0200

    created some basics function - not fully made yet.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # PythonCLI
+This PythonCLI makes easir work with images. Blur, rotate, put text and many more! No more learning hard programs like Photoshop!
 
-## CLI that make easier to work with files.
+## How to set up
 
-## Develop in progress no docs availibe yet.
+1. Clone this repo.
+2. Move to this directory.
+3. Type: python main.py -p "pathToImage" + options(read more info below)
+
+## Options
+
+`-b` - Blurs image
+`-ra` - Blurs image for some radius. Must be used with -b flag.
+
+Text flag is included but not tested yet.
+
+## Contribution
+
+Contributions are welcome! Start by forking this repo, making changes and creating a pull request! If you have any more question or just want to talk, contact me please on email rojtomas@email.cz.
+
+## About
+
+Created by @TomasRoj on 27. April on rainy saturday on living room sofa.
+
+Tomáš Roj, rojtomas@email.cz
+This and other my projects are avilibe under MIT license. See LICENSE file for more info.
--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import click
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
+# List of commands
 @click.command()
-@click.option("-w", "--write", type=str, default="Hello from PythonCLI!", help="Writes some text on command line")
-@click.option("-openFile", "--openFile", type=str, help="opens file from path")
+@click.option("-p", "--path", type=str, help="Sets path of destination image.")
+@click.option("-b", "--blur", default=False, type=bool, help="Blurs an image.")
+@click.option("-ra", "--radius", default=0, type=int, help="Sets blur radius for x.")
+@click.option("-text", "--text", default="Text", type=str, help="Writes some text on image.")
 
-def writeText(write):
-    print(write)
+def writeText(path, text):
+    try:
+        original = Image.open(path)
+        withText = original.ImageDraw.Draw(original)
+        font = ImageFont.truetype("sans-serif.ttff", 25)
 
-def OpenFile(openFile, path):
-    file = open(path)
-    file.write("opened")
+        withText.text((0, 0), text, (255, 255, 255), font=font)
+        withText.save("picture-with-text.jpg")
+    except:
+        print("An error ocurred somewhere :-(")
+
+def process(path, blur, radius):
+    try:
+        # Load an image from the hard drive
+        original = Image.open(path)
+
+        # Blur the image
+        blurred = original.filter(ImageFilter.GaussianBlur(radius))
+
+        # save the new image
+        blurred.save("blurred.png")
+        print("Action completed!")
+
+    except:
+        print("Unable to load image")
